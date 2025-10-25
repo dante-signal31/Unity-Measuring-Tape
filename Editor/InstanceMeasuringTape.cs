a66c624c2c3788a0447c744dc78ec1a7e9c3c244
dante-signal31 <dante.signal31@gmail.com>
2025-10-25 14:46:02 +0200

    New script to instance tapes from menu.

---
+++ Editor/InstanceMeasuringTape.cs
@@ -0,0 +1,33 @@
+﻿
+
+using UnityEditor;
+using UnityEngine;
+
+public class InstanceMeasuringTape
+{
+    private static readonly string _prefabPath = 
+        "Unity Measuring Tape/Runtime/Prefabs/MeasuringTape.prefab"; 
+
+    [MenuItem("Tools/Add measure tape")] 
+    public static void InstanceTape()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(_prefabPath);
+        
+        if (prefab == null)
+        {
+            Debug.LogError($"Measuring tape prefab not found at: {_prefabPath}");
+            return;
+        }
+        
+        GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        if (instance == null) 
+        {
+            Debug.LogError($"Measuring tape was not instanced.");
+            return;
+        }
+        
+        // Select the instance in the hierarchy to make it visible.
+        Selection.activeGameObject = instance;
+    }
+}
